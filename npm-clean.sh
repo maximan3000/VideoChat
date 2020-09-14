92f8ec3ea73d4e67929dc8cbd1dfc870b24b2bac
Maxim Petrov <grayen.job@gmail.com>
2020-09-14 11:45:40 +0300

    update .sh scripts

--- npm-clean.sh
+++ npm-clean.sh
@@ -1,9 +1,5 @@
-cd ./dev-tools \
+rm -rf ./node_modules \
+&& cd ./chat-client \
 && rm -rf ./node_modules \
-&& rm -f package-lock.json \
-&& cd ../chat-client \
-&& rm -rf ./node_modules \
-&& rm -f package-lock.json \
 && cd ../chat-server \
 && rm -rf ./node_modules \
-&& rm -f package-lock.json
--- npm-install.sh
+++ npm-install.sh
@@ -1,6 +1,5 @@
-cd ./dev-tools \
-&& npm install \
-&& cd ../chat-client \
+npm install \
+&& cd ./chat-client \
 && npm install \
 && cd ../chat-server \
 && npm install
